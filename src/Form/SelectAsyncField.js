5139f5171c9ca875d4b36197623b91ea939fec0f
Emanuele Ingrosso <ingro85@gmail.com>
2016-10-03 14:31:46 +0200

    Removed initialLabel props from SelectAsyncField

--- src/Form/SelectAsyncField.js
+++ src/Form/SelectAsyncField.js
@@ -5,16 +5,12 @@ import SelectAsync from '../SelectAsync';
 
 function SelectAsyncComponent(props) {
     // onBlur necessario altrimenti si resetta il valore del select al blur...
-    
-    const initialValue = props.initialLabel && props.meta.pristine && { id: props.input.value, name: props.initialLabel };
-
     return <SelectAsync
         className="form-control"
         placeholder={props.placeholder}
         loadOptions={props.loadOptions}
         {...props.input}
         {...props.selectOptions}
-        value={initialValue ? initialValue : props.input.value}
         onBlur={() => props.input.onBlur()}
     />;
 }
