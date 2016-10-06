81d230a94c160df53d94d73e11dd31ec03505cb5
Emanuele Ingrosso <ingro85@gmail.com>
2016-10-06 12:23:04 +0200

    Added storybook options addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
 import { configure, setAddon } from '@kadira/storybook';
 import infoAddon from '@kadira/react-storybook-addon-info';
+import { setOptions } from '@kadira/storybook-addon-options';
 
 import '../src/style.css';
+
+setOptions({
+    name: 'Vivi',
+    url: 'https://www.npmjs.com/package/vivi',
+    downPanelInRight: true
+});
 
 const req = require.context('../stories', true, /.js$/)
 
