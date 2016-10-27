c0e906989f3416547745ce02bb73cf9e32e8c8ce
Ingruz <ingro85@gmail.com>
2016-10-27 22:07:41 +0200

    Added en messages

---
+++ src/messages/en.js
@@ -0,0 +1,39 @@
+export default {
+    DatePicker: {
+        placeholder: 'Select date'
+    },
+    DateRangePicker: {
+        endDatePlaceholderText: 'Start date',
+        startDatePlaceholderText: 'End date'
+    },
+    LoadingButton: {
+        loadingMsg: 'Loading...'
+    },
+    Paginator: {
+        locale: {
+            items_per_page: 'items per page',
+            jump_to: 'Go to',
+            page: 'Page',
+            first_page: 'First page',
+            last_page: 'Last page',
+            prev_page: 'Next page',
+            next_page: 'Previous page',
+            prev_5: '5 Pages ahead',
+            next_5: '5 Pages behind'
+        }
+    },
+    Select: {
+        noResultsText: 'No result found',
+        placeholder: 'Select a value'
+    },
+    SelectAsync: {
+        loadingPlaceholder: 'Wait please...',
+        noResultsText: 'No result found',
+        placeholder: 'Select a value',
+        searchPromptText: 'Type to search',
+        searchingText: 'Searching...',
+    },
+    Table: {
+        noRowsText: 'No item to show'
+    }
+};
