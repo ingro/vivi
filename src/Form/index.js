f57e766a0184db63716ee490493e001e1a4ff9d0
Emanuele Ingrosso <ingro85@gmail.com>
2017-03-31 18:27:46 +0200

    Export InputGroupHoc from form's components

--- src/Form/index.js
+++ src/Form/index.js
@@ -4,3 +4,4 @@ export RadioGroupField from './RadioGroupField';
 export SelectField from './SelectField';
 export SelectAsyncField from './SelectAsyncField';
 export TextareaField from './TextareaField';
+export InputGroupHoc from './InputGroupHoc.js';
